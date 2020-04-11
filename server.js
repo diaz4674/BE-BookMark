2bd7ed54f40c5e17fc24117783ef51de8b838bed
miguel diaz <diaz4674@gmail.com>
2020-04-10 23:37:05 -0700

    cors update

--- server.js
+++ server.js
@@ -13,4 +13,11 @@ server.use(express.json())
 
 configureRoutes(server)
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
 module.exports = server
